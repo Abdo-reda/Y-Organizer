273ca70c0491a366f88aefef004186e7d6c0c967
Abdo-reda <abdo.reda@aucegypt.edu>
2025-09-08 00:01:44 +0300

    initla messy code, vibe coded my way to hell

---
+++ src/components/common/InfoCard.vue
@@ -0,0 +1,6 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <p> CARD STUFF </p>
+</template>
---
+++ src/views/TodayView.vue
@@ -0,0 +1,6 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <p> --- today view </p>
+</template>
